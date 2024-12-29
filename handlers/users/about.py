80110cfcfcbc84d535aeeec0a9ba9560111826f0
yakubov077 <yakubovakbar380>
2024-12-29 11:44:37 +0300

    Lotin_kiril

--- handlers/users/about.py
+++ handlers/users/about.py
@@ -5,5 +5,17 @@ from aiogram.filters import Command
 #about commands
 @dp.message(Command("about"))
 async def about_commands(message:Message):
-    await message.answer("nech narsa")
+    await message.answer("""Ushbu bot orqali siz o‘zbek tilidagi matnlarni Lotin-Kiril va Kiril-Lotin formatlari o‘rtasida osonlik bilan o‘gira olasiz. 😊
 
+Bot imkoniyatlari:
+- Lotin alifbosidan Kiril alifbosiga o‘girish.
+- Kiril alifbosidan Lotin alifbosiga o‘girish.
+
+Misollar:
+- Kiriting: 'Salom, qalesiz?' -> 'Салом, қалесиз?'
+- Kiriting: 'Салом, қалесиз?' -> 'Salom, qalesiz?'
+
+Loyiha yaratuvchisi: [Sizning ismingiz yoki bot yaratuvchi haqida ma’lumot]
+Qo‘shimcha ma’lumot uchun bog‘laning: [aloqa ma’lumotlari]
+
+Botdan foydalanayotganingiz uchun rahmat! 😊""")
--- handlers/users/start.py
+++ handlers/users/start.py
@@ -1,8 +1,8 @@
-from aiogram.types import Message
+from aiogram.types import Message, ReplyKeyboardRemove
 from loader import dp,db
 from aiogram.filters import CommandStart
-
-
+from aiogram.fsm.context import FSMContext
+from aiogram import F
 @dp.message(CommandStart())
 async def start_command(message:Message):
     full_name = message.from_user.full_name
@@ -11,4 +11,23 @@ async def start_command(message:Message):
         db.add_user(full_name=full_name,telegram_id=telegram_id) #foydalanuvchi bazaga qo'shildi
         await message.answer(text="Assalomu alaykum, botimizga hush kelibsiz")
     except:
-        await message.answer(text="Assalomu alaykum")
+        await message.answer(text=f"""Assalomu alaykum, <b>{full_name}</b>! 😊
+                             
+Lotin-Kiril va Kiril-Lotin matn o‘girish botiga xush kelibsiz!  
+Kerakli formatga matnni o‘girish uchun shunchaki xabar yuboring, men uni darhol o‘giraman.  
+
+👉 Misollar  
+                             
+🔹 Kiriting: <b>Salom, do'stim!</b>
+  Javob: Салом, дўстим!  
+
+🔹 Kiriting: <b>Салом, дўстим!</b>  
+  Javob: Salom, do'stim!
+                             
+Matnni yuboring va jarayonni kuzating! 😊
+""",reply_markup=ReplyKeyboardRemove(),parse_mode='html')
+
+@dp.message(~F.text)
+async def reklama_rasm_del(message:Message, state:FSMContext):
+    await message.delete()
+    await message.answer(text= "✖️ Faqat so'z kiriting !")
